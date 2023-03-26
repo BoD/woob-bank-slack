69522cfd4156a0d44d5cbdf06a32667531673355
BoD <BoD@JRAF.org>
2023-03-26 14:05:09 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile.FromInstruction
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm")
     kotlin("kapt")
     application
-    id("com.github.johnrengelman.shadow")
     id("com.bmuschko.docker-java-application")
 }
 
@@ -34,33 +32,6 @@ application {
     mainClass.set("MainKt")
 }
 
-tasks {
-    named<ShadowJar>("shadowJar") {
-        minimize()
-    }
-}
-
-// Implements https://github.com/brianm/really-executable-jars-maven-plugin maven plugin behaviour.
-// To check details how it works, see http://skife.org/java/unix/2011/06/20/really_executable_jars.html.
-tasks.register<DefaultTask>("shadowJarExecutable") {
-    description = "Creates a self-executable file, that runs the generated shadow jar"
-    group = "Distribution"
-
-    inputs.files(tasks.named("shadowJar"))
-    val origFile = inputs.files.singleFile
-    outputs.files(File(origFile.parentFile, origFile.nameWithoutExtension + "-executable"))
-
-    doLast {
-        val execFile: File = outputs.files.files.first()
-        val out = execFile.outputStream()
-        out.write("#!/bin/sh\n\nexec java -jar \"\$0\" \"\$@\"\n\n".toByteArray())
-        out.write(inputs.files.singleFile.readBytes())
-        out.flush()
-        out.close()
-        execFile.setExecutable(true, false)
-    }
-}
-
 docker {
     javaApplication {
         maintainer.set("BoD <BoD@JRAF.org>")
@@ -80,29 +51,33 @@ tasks.withType<DockerBuildImage> {
 }
 
 tasks.withType<Dockerfile> {
-    val originalInstructions = instructions.get().toMutableList()
-    val fromInstructionIndex = originalInstructions
-        .indexOfFirst { item -> item.keyword == FromInstruction.KEYWORD }
-    originalInstructions.removeAt(fromInstructionIndex)
-    val baseImage = FromInstruction(Dockerfile.From("adoptopenjdk/openjdk11-openj9:x86_64-ubuntu-jre-11.0.18_10_openj9-0.36.1"))
-    originalInstructions.add(0, baseImage)
-    instructions.set(originalInstructions)
+    // Use OpenJ9 instead of the default one
+    instructions.set(
+        instructions.get().map { item ->
+            if (item.keyword == FromInstruction.KEYWORD) {
+                FromInstruction(Dockerfile.From("adoptopenjdk/openjdk11-openj9:x86_64-ubuntu-jre-11.0.18_10_openj9-0.36.1"))
+            } else {
+                item
+            }
+        }
+    )
 
     // Install python
     runCommand(
-        """apt-get update && \
-    apt-get install -y software-properties-common && \
-    apt-get install -y python3-pip"""
+        """
+            apt-get update && \
+            apt-get install -y software-properties-common && \
+            apt-get install -y python3-pip
+        """.trimIndent()
     )
     // Install woob from source
     runCommand("apt-get install -y git")
     runCommand("git clone https://gitlab.com/woob/woob.git --depth 1")
-    runCommand("cd woob && ./setup.py install")
+    runCommand("cd woob && pip install .")
     runCommand("woob update")
     environmentVariable("MALLOC_ARENA_MAX", "4")
 }
 
 
 // `./gradlew refreshVersions` to update dependencies
-// `./gradlew shadowJarExecutable` to build the "really executable jar"
 // `DOCKER_USERNAME=<your docker hub login> DOCKER_PASSWORD=<your docker hub password> ./gradlew dockerPushImage` to build and push the image
